b4ba088732ff81b43713bd1d15c884bf4238d519
Mike Spaans <99069233@mydavinci.nl>
2021-09-27 11:09:18 +0200

    min en max

--- meten.py
+++ meten.py
@@ -3,12 +3,20 @@ b = int(input('geef het tweede hele getal '))
 
 if a > b:
     max = a
+    min = b
     print (" a is het grootste getal: " +  str(max)  ) 
+    print ("het minimum is " + str(min))
+    print (" het maximum is " + str(max))
 
 elif a < b:
     min = a 
+    max = b
     print ("a is het kleinste getal " + str(min) )
-
+    print (" het minimum is " + str(min))
+    print (" het maximum is " + str(max))
 else:
     print ("a en b zijn even groot ")
-    
+    max = a
+    min = b
+    print (" het minimum is " + str(min))
+    print ("het maximum is " + str(max))
